ca63275cb4dc9c9841f8f631762a7126d98cc979
José Coelho <jose.coelho@palomagroup.com>
2024-12-06 21:50:22 +1300

    Add analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "goatcounter",
+      websiteId: "josecoelho",
     },
     locale: "en-US",
     baseUrl: "jose.co.nz",
